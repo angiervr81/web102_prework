708ca5c7e336ab3258a5cc8059f5ff7d475606d5
Angie <Angie.Rivera81@bcmail.cuny.edu>
2025-05-11 13:25:17 -0400

    Update index.js
     * Challenge 6: Add more information at the top of the page about the company.
     * Skills used: template literals, ternary operator

--- index.js
+++ index.js
@@ -152,13 +152,15 @@ allBtn.addEventListener("click", showAllGames);
 const descriptionContainer = document.getElementById("description-container");
 
 // use filter or reduce to count the number of unfunded games
-
+const unfundedGamesCount = GAMES_JSON.filter(game => game.pledged < game.goal).length;
 
 // create a string that explains the number of unfunded games using the ternary operator
-
+const unfundedString = `A total of $${totalRaised.toLocaleString()} has been raised for ${totalGames} game${totalGames === 1 ? '' : 's'}. Currently, ${unfundedGamesCount} game${unfundedGamesCount === 1 ? '' : 's'} remain unfunded. We need your help to fund these amazing games!`;
 
 // create a new DOM element containing the template string and append it to the description container
-
+const descriptionText = document.createElement("p");
+descriptionText.innerHTML = unfundedString;
+descriptionContainer.appendChild(descriptionText);
 /************************************************************************************
  * Challenge 7: Select & display the top 2 games
  * Skills used: spread operator, destructuring, template literals, sort 
